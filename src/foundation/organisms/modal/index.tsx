bfc70e943a7cf35813b3857c0ad72d56fb07005b
15akash <ajindal1512@gmail.com>
2024-09-14 20:18:54 +0530

    closing modal on click of escape keypress

--- src/foundation/organisms/modal/index.tsx
+++ src/foundation/organisms/modal/index.tsx
@@ -1,10 +1,26 @@
-import React, { FC } from "react";
+import { FC, useEffect } from "react";
 import { ModalProps } from "../../../models/model";
 import styles from "./Modal.module.scss";
 import { useCatImage } from "../../../utils/hook";
 
 const Modal: FC<ModalProps> = (props: ModalProps) => {
     const catImage = useCatImage(props.position);
+
+    useEffect(() => {
+        const handleKeyDown = (event: KeyboardEvent) => {
+            if (event.key === "Escape") {
+                props.isClose();
+            }
+        };
+        window.addEventListener("keydown", handleKeyDown);
+
+        // Cleanup function to remove the event listener when the component unmounts
+        return () => {
+            window.removeEventListener("keydown", handleKeyDown);
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return (
         <div className={styles["modal"]}>
             <div className={styles["modal--container"]}>
